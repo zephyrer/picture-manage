d2871b4713360f6390f3126b07eb7fb847da070c
hezhanbang@gmail.com <hezhanbang@gmail.com@db396d69-d92c-a7f9-00ee-95cd39429b86>
2013-02-21 23:05:29 +0000

    修改“文件”和“图片”下拉菜单。

--- trunk/PictureManage/Resource.h
+++ trunk/PictureManage/Resource.h
@@ -2,20 +2,47 @@
 // Microsoft Visual C++ generated include file.
 // Used by PictureManage.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_PictureManageTYPE				130
-#define ID_WINDOW_MANAGER			131
-#define IDS_WINDOWS_MANAGER			132
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_PictureManageTYPE           130
+#define ID_WINDOW_MANAGER               131
+#define IDS_WINDOWS_MANAGER             132
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_FILE_FALST_IMPORT            32774
+#define ID_FILE_FAST_IMPORT             32775
+#define ID_FILE_IMPORT                  32776
+#define ID_FILE_PASTE                   32777
+#define ID_FILE_ALL_SAVE                32778
+#define ID_32779                        32779
+#define ID_DEVICE_SCANX                 32780
+#define ID_DEVICE_SOURCE                32781
+#define ID_DEVICE_SCAN                  32782
+#define ID_DEVICE_                      32783
+#define ID_DEVICE_MOUTH                 32784
+#define ID_32785                        32785
+#define ID_DEVICE_NOTIFY                32786
+#define ID_PRINT                        32787
+#define ID_32788                        32788
+#define ID_FILE_PRINTF_                 32789
+#define ID_EXPORT_PPT                   32790
+#define ID_32791                        32791
+#define ID_IMAGE_                       32792
+#define ID_IMAGE_DESCRIPB               32793
+#define ID_IMAGE_DELETE                 32794
+#define ID_IMAGE_ALIGN                  32795
+#define ID_IMAGE_FIND                   32796
+#define ID_IMAGE_RENAME                 32797
 
-// 新对象的下一组默认值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32798
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
