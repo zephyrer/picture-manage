ae50167380af314bc14bcb0243a7d412b80e3d78
hezhanbang <hezhanbang@gmail.com>
2013-02-21 00:04:49 +0000

    把工程导入google版本库。

---
+++ PictureManage/PictureManage.h
@@ -0,0 +1,32 @@
+
+// PictureManage.h : PictureManage 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CPictureManageApp:
+// 有关此类的实现，请参阅 PictureManage.cpp
+//
+
+class CPictureManageApp : public CWinApp
+{
+public:
+	CPictureManageApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPictureManageApp theApp;
---
+++ PictureManage/PictureManageDoc.h
@@ -0,0 +1,41 @@
+
+// PictureManageDoc.h : CPictureManageDoc 类的接口
+//
+
+
+#pragma once
+
+
+class CPictureManageDoc : public CDocument
+{
+protected: // 仅从序列化创建
+	CPictureManageDoc();
+	DECLARE_DYNCREATE(CPictureManageDoc)
+
+// 属性
+public:
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+
+// 实现
+public:
+	virtual ~CPictureManageDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
