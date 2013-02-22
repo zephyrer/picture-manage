a2628aba464f10f242f1f2e8bc6605a767bd6359
hezhanbang <hezhanbang@gmail.com>
2013-02-22 01:30:18 +0000

    personInfo可以编译.

--- PictureManage/PersonInfo.cpp
+++ PictureManage/PersonInfo.cpp
@@ -1,35 +1,36 @@
-//#include "stdafx.h"
-//#include "PersonInfo.h"
-//
-//void DoctorInfo::RefreshFromDatabase()
-//{
-//
-//}
-//
-//void DoctorInfo::RefreshToDatabase()
-//{
-//
-//}
-//
-//DoctorInfo DoctorInfo::GetFromDatabase(int pid)
-//{
-//	DoctorInfo info;
-//	return info;
-//}
+#include "stdafx.h"
+#include "PersonInfo.h"
+
+void DoctorInfo::RefreshFromDatabase()
+{
+
+}
+
+void DoctorInfo::RefreshToDatabase()
+{
+
+}
+
+DoctorInfo DoctorInfo::GetFromDatabase(int pid)
+{
+	DoctorInfo info;
+	return info;
+}
+
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-//void PatientInfo::RefreshFromDatabase()
-//{
-//
-//}
-//
-//void PatientInfo::RefreshToDatabase()
-//{
-//
-//}
-//
-//PatientInfo PatientInfo::GetFromDatabase(int pid)
-//{
-//	PatientInfo info;
-//	return info;
-//}
+void PatientInfo::RefreshFromDatabase()
+{
+
+}
+
+void PatientInfo::RefreshToDatabase()
+{
+
+}
+
+PatientInfo PatientInfo::GetFromDatabase(int pid)
+{
+	PatientInfo info;
+	return info;
+}
